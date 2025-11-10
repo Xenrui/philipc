c4d7701c765ef0466707d41914ad1b6c08de033c
Xenon <joshua123.jdr@gmail.com>
2025-11-10 20:12:48 +0800

    fix(ui): adjusted button hover classes

--- app/components/home-banner.tsx
+++ app/components/home-banner.tsx
@@ -13,7 +13,7 @@ const Banner = () => {
 					</span>
 				</div>
 				<div className="">
-					<button className="flex bg-light-primary hover:bg-gray-300 text-dark-primary hover:text-cyan-700 hover:border-cyan-700 lg:w-50 py-2 px-5 rounded-4xl border-4 border-dark-primary  hover:cursor-pointer">
+					<button className="flex bg-light-primary hover:bg-dark-primary border-[#33444A] hover:border-dark-primary text-[#33444A] hover:text-light-primary lg:w-50 py-2 px-5 rounded-4xl border-4 hover:cursor-pointer">
 						<span className="m-auto font-extrabold text-xl">Buy now</span>
 						<MoveRight className="inline w-10 h-10 m-auto " />
 					</button>
@@ -23,8 +23,10 @@ const Banner = () => {
 			{/* Right */}
 			<div className="hidden md:inline relative z-0 bg-[#7AA2B8] w-4xl rounded-r-3xl ">
 				<div className="absolute bg-[#A3D4D3] w-fit h-30 -inset-x-2 inset-y-15 flex p-3 border-b-4 border-gray-500">
-					<ShieldCheck className="w-15 h-15 lg:w-17 lg:h-17 m-auto text-gray-600"/>
-					<span className="m-auto mask-linear-from-neutral-800 font-extrabold text-3xl lg:text-4xl text-gray-600">Shop with Confidence</span>
+					<ShieldCheck className="w-15 h-15 lg:w-17 lg:h-17 m-auto text-gray-600" />
+					<span className="m-auto mask-linear-from-neutral-800 font-extrabold text-3xl lg:text-4xl text-gray-600">
+						Shop with Confidence
+					</span>
 				</div>
 			</div>
 		</div>
